c107eb808d57ea066b9f04113d729033f0b7ea29
Andrew <liboan1997@gmail.com>
2017-07-24 13:12:58 -0700

    successfully plays back mp3 files

--- feature-phone-prototype/run.py
+++ feature-phone-prototype/run.py
@@ -1,18 +1,25 @@
 """Using Twilio's Python Quickstart guide, found at
+https://www.twilio.com/docs/quickstart/python/twiml
 """
 
-from flask import Flask
+from flask import Flask, url_for, send_from_directory, request
 from twilio.twiml.voice_response import VoiceResponse
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='')
+
 
 @app.route("/", methods=['GET', 'POST'])
 def hello_monkey():
     """Respond to incoming requests."""
     resp = VoiceResponse()
-    resp.say("Hello Monkey")
-
+    resp.say("Welcome to Malasakit.")
+    resp.play("%s%s" % (request.url, 'mp3/q01.mp3'))
     return str(resp)
+
+@app.route('/mp3/<path:path>')
+def send_mp3(path):
+    print("asdf", path)
+    return send_from_directory('mp3', path)
 
 if __name__ == "__main__":
     app.run(debug=True)
