f6bef9754c40978560aa9efae1aff30a6f14ab0c
Andrew <liboan1997@gmail.com>
2017-07-24 13:42:26 -0700

    prototype plays a question, records user response, downloads

--- feature-phone-prototype/run.py
+++ feature-phone-prototype/run.py
@@ -2,6 +2,8 @@
 https://www.twilio.com/docs/quickstart/python/twiml
 """
 
+import urllib
+
 from flask import Flask, url_for, send_from_directory, request
 from twilio.twiml.voice_response import VoiceResponse
 
@@ -13,13 +15,31 @@ def hello_monkey():
     """Respond to incoming requests."""
     resp = VoiceResponse()
     resp.say("Welcome to Malasakit.")
-    resp.play("%s%s" % (request.url, 'mp3/q01.mp3'))
+    resp.say("This is a survey in which you will be asked several questions. You will be asked to rate each response verbally by saying a number, zero to nine.")
+
+    play_quant_question(resp, 1, 'q01.mp3')
     return str(resp)
 
 @app.route('/mp3/<path:path>')
 def send_mp3(path):
     print("asdf", path)
     return send_from_directory('mp3', path)
+
+@app.route('/handle-recording', methods=['GET', 'POST'])
+def handle_recording():
+    resp = VoiceResponse()
+    recording_url = request.values.get('RecordingUrl', None)
+    user_response = urllib.URLopener()
+    user_response.retrieve(recording_url, 'responses/response.mp3')
+    resp.say("Response received.")
+    return str(resp)
+
+
+def play_quant_question(resp, question_id, question_path):
+    resp.say("Please answer Question %s by saying a number from 0 to 9." % question_id)
+    resp.play("%smp3/%s" % (request.url, question_path))
+    resp.record(maxLength='3', action='/handle-recording')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
