f43d4c78d648bad88f1ff8a1c27f9e227ad6af28
Andrew <liboan1997@gmail.com>
2017-07-28 17:15:58 -0700

    added question

--- feature-phone-prototype/run.py
+++ feature-phone-prototype/run.py
@@ -2,9 +2,10 @@
 https://www.twilio.com/docs/quickstart/python/twiml
 """
 
+import time
 import urllib
 
-from flask import Flask, url_for, send_from_directory, request
+from flask import Flask, url_for, send_from_directory, request, redirect
 from twilio.twiml.voice_response import VoiceResponse
 
 app = Flask(__name__, static_url_path='')
@@ -15,9 +16,9 @@ def hello_monkey():
     """Respond to incoming requests."""
     resp = VoiceResponse()
     resp.say("Welcome to Malasakit.")
-    resp.say("This is a survey in which you will be asked several questions. You will be asked to rate each response verbally by saying a number, zero to nine.")
-
-    play_quant_question(resp, 1, 'q01.mp3')
+    resp.say("Here you will be asked several questions about typhoon preparedness.")
+    resp.pause(1)
+    resp.redirect(url_for('play_quant_question'))
     return str(resp)
 
 @app.route('/mp3/<path:path>')
@@ -32,14 +33,38 @@ def handle_recording():
     user_response = urllib.URLopener()
     user_response.retrieve(recording_url, 'responses/response.mp3')
     resp.say("Response received.")
+    resp.redirect(url_for('play_qual_question'))
+
     return str(resp)
 
 
-def play_quant_question(resp, question_id, question_path):
-    resp.say("Please answer Question %s by saying a number from 0 to 9." % question_id)
-    resp.play("%smp3/%s" % (request.url, question_path))
-    resp.record(maxLength='3', action='/handle-recording')
+@app.route('/handle-recording-qual', methods=['GET', 'POST'])
+def handle_recording_qual():
+    resp = VoiceResponse()
+    recording_url = request.values.get('RecordingUrl', None)
+    user_response = urllib.URLopener()
+    user_response.retrieve(recording_url, 'responses/response_qual.mp3')
+    resp.say("Response received.")
+    resp.pause(1)
+    resp.say("Thanks for participating in Malasakit.")
+    resp.pause(0.5)
+    return str(resp)
 
+@app.route('/play-quant-question', methods=['GET','POST'])
+def play_quant_question():
+    resp = VoiceResponse()
+    resp.say("Please answer Question %s by saying a number from 0 to 9." % 1)
+    resp.play("%smp3/%s" % (request.url_root, 'q01.mp3'))
+    resp.record(maxLength='1', action='/handle-recording')
+    return str(resp)
+
+@app.route('/play-qual-question', methods=['GET', 'POST'])
+def play_qual_question():
+    resp = VoiceResponse()
+    resp.say("This is an open-ended question. You have up to 15 seconds to record a response.")
+    resp.play("%smp3/%s" % (request.url_root, 'qual.mp3'))
+    resp.record(maxLength='15', action='/handle-recording-qual')
+    return str(resp)
 
 if __name__ == "__main__":
     app.run(debug=True)
